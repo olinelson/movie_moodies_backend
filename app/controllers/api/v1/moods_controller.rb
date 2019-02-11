a4af2d60ece0faa9fb821c708e2a15eb18f9c05f
Oli Nelson <olivernelson@icloud.com>
2019-02-11 11:56:23 -0500

    models up and running rendering json

--- app/controllers/api/v1/moods_controller.rb
+++ app/controllers/api/v1/moods_controller.rb
@@ -1,2 +1,29 @@
 class Api::V1::MoodsController < ApplicationController
+  def index
+    @moods = Mood.all
+    render json: @moods
+
+  end
+
+  def show
+    @mood = Mood.find(params[:id])
+    render json: @mood
+  end
+
+  def update
+    @mood.update(mood_params)
+    if @mood.save
+      render json: @mood, status: :accepted
+    else
+      render json: {errors: @mood.errors.full_messages }, status: :unprocessible_entity
+    end
+  end
+
+  private
+
+  def mood_params
+    params.permit( :name, :image)
+  end
+
+  
 end
--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -1,2 +1,32 @@
 class Api::V1::MoviesController < ApplicationController
+  def index
+    @movies = Movie.all
+    render json: @movies
+
+  end
+
+  def show
+    @mood = Mood.find(params[:id])
+    render json: @mood
+  end
+
+  def update
+    @movie.update(movie_params)
+    if @movie.save
+      render json: @movie, status: :accepted
+    else
+      render json: {errors: @movie.errors.full_messages }, status: :unprocessible_entity
+    end
+  end
+
+  private
+
+  def movie_params
+    params.permit( :title, :length, :release, :image, :description)
+  end
+
+  def find_movie
+    @move = Movie.find(params[:id])
+  end
+
 end
