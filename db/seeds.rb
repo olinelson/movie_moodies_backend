bb62863d2de52779eae9b5d729567d95e0da7f53
Oli Nelson <olivernelson@icloud.com>
2019-02-12 16:48:44 -0500

    added more api content and font awesome emoji

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,21 @@
+combinedCalls = []
+
+mostPopularMovies1 = ::RestClient::Request.execute(method: :get, url: "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=d2001c75a6bc64e98cc457d9b2a86444&page=1", headers: {'Content-Type': 'application/json', 'Accept': 'application/json'})
+hash1 = JSON.parse(mostPopularMovies1)
+combinedCalls.push(hash1["results"])
 
 
-mostPopularMovies = ::RestClient::Request.execute(method: :get, url: "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=d2001c75a6bc64e98cc457d9b2a86444', headers: {'Content-Type': 'application/json', 'Accept': 'application/json"})
-hash = JSON.parse(mostPopularMovies)
-movies = hash["results"]
+mostPopularMovies2 = ::RestClient::Request.execute(method: :get, url: "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=d2001c75a6bc64e98cc457d9b2a86444&page=2", headers: {'Content-Type': 'application/json', 'Accept': 'application/json'})
+hash2 = JSON.parse(mostPopularMovies2)
+combinedCalls.push(hash2["results"])
+
+mostPopularMovies3 = ::RestClient::Request.execute(method: :get, url: "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=d2001c75a6bc64e98cc457d9b2a86444&page=2", headers: {'Content-Type': 'application/json', 'Accept': 'application/json'})
+hash3 = JSON.parse(mostPopularMovies3)
+combinedCalls.push(hash3["results"])
+
+movies = combinedCalls.flatten
+
+
 imageUrl= "https://image.tmdb.org/t/p/w500"
 
 movies.each do |movie|
@@ -14,15 +27,49 @@ end
 # 50.times do
 #   Movie.create(title: Faker::Book.title, length: rand(20..240), release: Faker::Date.birthday(1, 50), image:"https://3.bp.blogspot.com/-NR5TQG15EQU/UrCYGi7FYDI/AAAAAAAAARQ/bUh7usV1HFY/s1600/casablanca-poster-artwork-humphrey-bogart-ingrid-bergman-paul-henreid.jpg", description: "Quo sunt voluptatem mollitia libero vel aut est. Ut ea quis aliquam eum voluptatem odio sed eaque. Quidem laboriosam iste laboriosam voluptatibus. Aperiam est commodi tempore totam. Nihil quaerat dolores eius aliquid libero delectus beatae eos.")
 # end
+ moods = [
+   {
+     name: "happy",
+     icon: "far fa-smile"
+   },
+   {
+     name: "sad",
+     icon: "fas fa-frown"
+   },
+   {
+     name: "scary",
+     icon: "far fa-surprise"
+   },
+   {
+     name: "funny",
+     icon: "fas fa-grin-squint-tears"
+   },
+   {
+     name: "angry",
+     icon: "far fa-angry"
+   },
+   {
+     name: "bored",
+     icon: "fas fa-meh-rolling-eyes"
+   },
+   {
+     name: "romantic",
+     icon: "fas fa-grin-hearts"
+   },
+   {
+     name: "bemused",
+     icon: "fas fa-grin-tongue-wink"
+   },
+   {
+     name: "snarky",
+     icon: "fas fa-grin-tongue-squint"
+   },
+ ]
 
- moodNames = ["Happy", "Sad", "Judgemental", "Disappointed", "Mad", "Anxious", "Depressed", "Bored", "Optimistic", "Inspired", "Excited", "Amused", "Energetic"]
-
-
-
- moodNames.each do |moodName|
-   Mood.create( name: moodName )
+ moods.each do |mood|
+   Mood.create( name: mood[:name], image: mood[:icon] )
  end
 
- 100.times do
-   MovieMood.create(movie_id: Movie.all.sample.id, mood_id: Mood.all.sample.id)
- end
+Movie.all.each do |movie|
+  MovieMood.create(movie_id: movie.id, mood_id: Mood.all.sample.id)
+end
